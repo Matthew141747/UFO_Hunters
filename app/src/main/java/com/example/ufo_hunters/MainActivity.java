30fdf209bc6fd54a20f8fda988362604dd0b68f4
Hemsankha <royhemsankha@gmail.com>
2023-04-17 14:26:01 +0100

    added google maps to the home page but for some reason the app crashes after login/registration

--- app/src/main/java/com/example/ufo_hunters/MainActivity.java
+++ app/src/main/java/com/example/ufo_hunters/MainActivity.java
@@ -1,22 +1,38 @@
 package com.example.ufo_hunters;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class MainActivity extends AppCompatActivity {
+import org.checkerframework.checker.nullness.qual.NonNull;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     FirebaseAuth auth;
     Button button, buttonB, buttonComunity;
     TextView textView;
     FirebaseUser user;
+
+    GoogleMap myMap;
+
+    private final int FINE_PERMISSION_CODE = 1;
+    Location currentLocation;
+    FusedLocationProviderClient fusedLocationProviderClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,18 +40,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         auth = FirebaseAuth.getInstance();
-        button= findViewById(R.id.logout);
+        button = findViewById(R.id.logout);
         buttonB = findViewById(R.id.Profile);
         textView = findViewById(R.id.user_details);
         buttonComunity = findViewById(R.id.Community);
 
         user = auth.getCurrentUser();
-        if(user == null){
+        if (user == null) {
             Intent intent = new Intent(getApplicationContext(), Login.class);
             startActivity(intent);
             finish();
-        }
-        else{
+        } else {
             textView.setText(user.getEmail());
         }
 
@@ -66,5 +81,21 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+  //  fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+    //getLastLocation();
+
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap){
+        myMap = googleMap;
+        LatLng dublin = new LatLng(53, -6);
+        myMap.addMarker(new MarkerOptions().position(dublin).title("dublin"));
+        myMap.moveCamera(CameraUpdateFactory.newLatLng(dublin));
+
     }
 }
--- app/src/main/java/com/example/ufo_hunters/Profile.java
+++ app/src/main/java/com/example/ufo_hunters/Profile.java
@@ -1,13 +1,13 @@
 package com.example.ufo_hunters;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -54,6 +54,14 @@ public class Profile extends AppCompatActivity {
             public void onClick(View view) {
                 FirebaseAuth.getInstance().signOut();
                 Intent intent = new Intent(getApplicationContext(), Login.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(intent);
                 finish();
             }
